de2107bcbe3bc91cfe02f0f63fe7a98a6569e653
chrisstiles <chrisstiles92@gmail.com>
2015-11-12 17:25:53 -0800

    Configuration to serve static asset

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Invoicer
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Serve assets in Heroku
+    config.serve_static_assets = true
   end
 end
