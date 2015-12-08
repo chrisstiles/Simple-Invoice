91da57a24248e5448d9b748da6f87dd8a39bdf49
chrisstiles <chrisstiles92@gmail.com>
2015-12-07 20:08:48 -0800

    Add path

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,7 @@
-WickedPdf.config = {
-  #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
-  #:layout => "pdf.html",
-  :exe_path => '/usr/local/bin/wkhtmltopdf'
-}
+bin_location = case Rails.env
+  when 'production' then "#{Rails.root}/bin/wkhtmltopdf"
+  when 'development' then "/usr/local/bin/wkhtmltopdf"
+  else `which wkhtmltopdf`
+end
+
+WickedPdf.config = { :exe_path => bin_location }
