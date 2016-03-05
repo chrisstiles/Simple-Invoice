1588179364edf0ab5fae4fd1ed48e20422c93644
chrisstiles <chrisstiles92@gmail.com>
2016-03-04 19:03:58 -0800

    Bugfix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,14 @@ module ApplicationHelper
 		end
 	end
 
+	def pdf_image_tag(image)
+		if Rails.env.production?
+			image_tag image.logo.gsub('https', 'http')
+		else
+			wicked_pdf_image_tag image.logo.gsub('https', 'http')
+		end
+	end
+
 	def get_terms
 		(@invoice.due_date - @invoice.date).to_i
 	end
