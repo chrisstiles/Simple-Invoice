f1e9dffaee264cbbb0b9a8c5537e78ff82762b3b
chrisstiles <chrisstiles92@gmail.com>
2016-03-13 17:44:44 -0700

    Attempt font fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,13 @@ module ApplicationHelper
 		end
 	end
 
+	def asset_data_base64(path)
+		asset = Rails.application.assets.find_asset(path)
+		throw "Could not find asset '#{path}'" if asset.nil?
+		base64 = Base64.encode64(asset.to_s).gsub(/\s+/, "")
+		"data:#{asset.content_type};base64,#{Rack::Utils.escape(base64)}"
+	end
+
 	def pdf_image_tag(image)
 		unless image.logo.blank?
 			if Rails.env.production?
