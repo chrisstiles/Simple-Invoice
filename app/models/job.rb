1884cd4cf06d6ac13d76398155f17840bb1c0bb2
chrisstiles <chrisstiles92@gmail.com>
2016-06-26 22:25:19 -0700

    Minor bugfixes

--- app/models/job.rb
+++ app/models/job.rb
@@ -8,7 +8,7 @@ class Job < ActiveRecord::Base
 	            numericality: { greater_than_or_equal_to: 0, message: "^Job quantity must be a number greater than or equal to 0", allow_blank: true }
 
   validates :job_rate, presence: true, 
-	            numericality: { greater_than_or_equal_to: 0, message: "^ Job Description must be a number greater than or equal to $0.00", allow_blank: true }
+	            numericality: { greater_than_or_equal_to: 0, message: "^ Job description must be a number greater than or equal to $0.00", allow_blank: true }
 
   # Length
   validates :job_description, length: { maximum: 500 }
@@ -28,9 +28,9 @@ class Job < ActiveRecord::Base
   def check_total(number, name)
     unless (0..999999999).include?(number)
       if name == "quantity"
-        errors.add(:job_quantity, "^Job Quantity cannot exceed 999,999,999")
+        errors.add(:job_quantity, "^Job quantity cannot exceed 999,999,999")
       else
-        errors.add(:job_rate, "^Job Rate cannot exceed $999,999,999.00")
+        errors.add(:job_rate, "^Job rate cannot exceed $999,999,999.00")
       end
     end
   end
