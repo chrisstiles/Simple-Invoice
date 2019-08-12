7f4513d5fb88365b373dca14250fbf8bd5b5e5e3
Chris Stiles <chrisstiles92@gmail.com>
2019-08-11 18:02:00 -0700

    Add www middleware back

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
   
-  # config.middleware.use Rack::WWW
+  config.middleware.use Rack::WWW
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
